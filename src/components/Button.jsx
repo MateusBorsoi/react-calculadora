a35a2f0790d3f38a8f282af0988d107a2423f74c
Mateus Borsoi <borsoimateus@gmail.com>
2023-05-14 14:51:44 -0300

    final

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./Button.css";
 
 const Button = (props) => {
-  return <button className="button">{props.label}</button>;
+  const { operation, double, triple, click, label } = props;
+
+  let classes = "button";
+  classes += operation ? " operation" : " ";
+  classes += double ? " double" : " ";
+  classes += triple ? " triple" : " ";
+
+  const handleClick = () => {
+    if (click) {
+      click(label);
+    }
+  };
+
+  return (
+    <button onClick={handleClick} className={classes}>
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
